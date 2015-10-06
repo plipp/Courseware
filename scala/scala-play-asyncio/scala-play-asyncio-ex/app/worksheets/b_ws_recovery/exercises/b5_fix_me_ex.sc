7196f79bda04557fe8e2cb5349984b8bbae0c786
plipp <p.lipp@web.de>
2015-10-06 23:24:55 +0200

    some refactoring

--- scala/scala-play-asyncio/scala-play-asyncio-ex/app/worksheets/b_ws_recovery/exercises/b5_fix_me_ex.sc
+++ scala/scala-play-asyncio/scala-play-asyncio-ex/app/worksheets/b_ws_recovery/exercises/b5_fix_me_ex.sc
@@ -1,18 +1,1 @@
-import java.util.concurrent.TimeUnit
-
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
-
-// ------------------------------------------------------------------
-def doSomethingDangerous (i:Int): Future[Int] = {
-  if (i%2==0) throw new IllegalArgumentException("not divisible by 2")
-  else Future.failed(new IllegalArgumentException("something else is wrong"))
-}
-
-val value = 45
-val future: Future[Int] = doSomethingDangerous(value)
-val result = Await.result(future, Duration(2, TimeUnit.SECONDS))
-
-// ------------------------------------------------------------------
-// fix it, so that for all 'value's in case of any error 0 is returned
+// --> see unit.futures.b.FutureRecoverySpec
---
+++ scala/scala-play-asyncio/scala-play-asyncio-ex/test/unit/futures/b/FutureRecoverySpec.scala
@@ -0,0 +1,42 @@
+package unit.futures.b
+
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.util.Try
+
+@RunWith(classOf[JUnitRunner])
+class FutureRecoverySpec extends Specification {
+
+  def givenDoSomethingDangerous(i: Int): Future[Int] = {
+    if (i % 2 == 0) throw new IllegalStateException
+    else if (i % 3 == 0) Future.failed(new IllegalArgumentException)
+    else Future.successful(i)
+  }
+
+  // code under test: FIXME
+  def consumeSomeThingDangerous(i: Int): Future[Int] = givenDoSomethingDangerous(i)
+
+  "consumeSomeThingDangerous" should {
+    "multiply allowed values with 2" in {
+      val result = Await.result(consumeSomeThingDangerous(11), Duration.Inf)
+      result mustEqual 22
+    }
+    "return -1 in case of multiples of 2" in {
+      val result = Await.result(consumeSomeThingDangerous(14), Duration.Inf)
+      result mustEqual -1
+    }
+    "return -1 in case of multiples of 3" in {
+      val result = Await.result(consumeSomeThingDangerous(9), Duration.Inf)
+      result mustEqual -1
+    }
+    "return -1 in case of multiples of 2 and 3" in {
+      val result = Await.result(consumeSomeThingDangerous(6), Duration.Inf)
+      result mustEqual -1
+    }
+  }
+}
