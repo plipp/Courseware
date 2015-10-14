f0587170e3c67b31c3cc73f584814093a37a75e4
Patricia Lipp <p.lipp@web.de>
2015-10-14 23:12:46 +0200

    new courseware stuff

---
+++ scala/scala-play-asyncio/scala-play-asyncio-ex/test/unit/futures/b/FutureCombinationSpec1.scala
@@ -0,0 +1,68 @@
+package unit.futures.b
+
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import org.specs2.specification.BeforeAfterAll
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+// requires extra library
+
+@RunWith(classOf[JUnitRunner])
+class FutureCombinationSpec1 extends Specification with BeforeAfterAll {
+
+
+  def computeSquare(i: Int): Future[Int] = ???
+
+  // test
+  "computeSquare" should {
+    "square 2" in {
+      val result = Await.result(computeSquare(2), Duration.Inf)
+      result mustEqual 4
+    }
+  }
+
+  // -------------------------------------------------------------------
+
+  def computeSquare(i: Future[Int]): Future[Int] = ???
+
+  // test
+  "computeSquare" should {
+    "square 2" in {
+      val result = Await.result(computeSquare(Future(2)), Duration.Inf)
+      result mustEqual 4
+    }
+  }
+
+  // -------------------------------------------------------------------
+
+  class SumSequence(start: Int, stop: Int, delay: Long = 0) {
+    def perform(): Int = {
+      require(start >= 0)
+      Thread.sleep(delay)
+      (start to stop).sum
+    }
+  }
+
+  def findSumOfSums (sums: Seq[SumSequence]): Future[Int] = {
+    ???
+  }
+
+  // test
+  "findSumOfSums" should {
+    "return 100" in {
+      val delayedSumSeq1 = new SumSequence(0,100,2000)
+      val delayedSumSeq2 = new SumSequence(0,100,1000)
+
+      val result = Await.result(findSumOfSums(Seq(delayedSumSeq1, delayedSumSeq2)), Duration.Inf)
+      result mustEqual 2*5050
+    }
+  }
+
+  override def beforeAll(): Unit = println ("BEGIN Spec")
+
+  override def afterAll(): Unit = println ("END  Spec")
+}
---
+++ scala/scala-play-asyncio/scala-play-asyncio-full/app/worksheets/b_ws_recovery/b5_promises.sc
@@ -0,0 +1,25 @@
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future, Promise}
+
+val f98: Future[Int] = Future.successful(98)
+val f99: Future[Int] =  Future {
+//  Thread.sleep(500)
+  100
+}
+
+// combines futures
+def first[T](f: Future[T], g: Future[T]): Future[T] = {
+  val p = Promise[T]()
+  f onSuccess {
+    case x => println ("f successful"); p.trySuccess(x)
+  }
+  g onSuccess {
+    case x => println ("g successful"); p.trySuccess(x)
+  }
+  p.future
+}
+
+val result = Await.result(first(f99, f98), Duration(2000, TimeUnit.SECONDS))
